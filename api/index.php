7f562f55aaa6343a1a9d04f0d87bcc1d40ed28c1
Daniel Oliveira <danielsapup3@gmail.com>
2022-11-01 11:42:20 -0300

    Update index.php

--- api/index.php
+++ api/index.php
@@ -2,6 +2,10 @@
 //Inclui o arquivo de conexão com o banco de dados
 include_once "./conexao.php";
 
+//Criar o cabeçalho para retornar JSON
+header("Content-type: application/json; charset=utf-8");
+
+
 //Buscar no banco de dados os produtos
 $query_produtos = "SELECT id, nome FROM produtos";
 //$query_produtos = "SELECT id, nome FROM produtos WHERE id = 100";
@@ -14,11 +18,14 @@ $result_produtos = $conn->prepare($query_produtos);
 $result_produtos->execute();
 
 if(($result_produtos) and ($result_produtos->rowCount() != 0)){
+    //Ler os registros retornado do banco de dados
     $produtos = $result_produtos->fetchAll();
 
-    var_dump($produtos);
+    //retornar os dados em formato JSON
+    echo json_encode(['status' => 200, 'dados' => $produtos]);
 }else{
-    echo "Erro: nenhum produto encontrado!";
+    //retornar os dados em formato JSON
+    echo json_encode(['status' => 404, 'msg' => "Erro: nenhum produto encontrado!"]);
 }
 
 
