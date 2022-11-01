c0dab12c310e354ad7d9f281d8103a058aaeab2d
Daniel Oliveira <danielsapup3@gmail.com>
2022-11-01 12:43:07 -0300

    Update

--- api/index.php
+++ api/index.php
@@ -1,34 +1,55 @@
 <?php 
-//Inclui o arquivo de conexão com o banco de dados
-include_once "./conexao.php";
 
-//Criar o cabeçalho para retornar JSON
-header("Content-type: application/json; charset=utf-8");
+// Incluir o arquivo com a conexao com banco de dados
+include_once './conexao.php';
 
+// Criar o cabeçalho para retornar XML
+header("Content-type: text/xml; charset=utf-8");
 
-//Buscar no banco de dados os produtos
+// SimpleXMLElement função do PHP que retorna string XML
+$produtosXML = new SimpleXMLElement("<produtos></produtos>");
+
+// Buscar no banco de dados os produtos
+//$query_produtos = "SELECT id, nome FROM produtos WHERE id = 1000";
 $query_produtos = "SELECT id, nome FROM produtos";
-//$query_produtos = "SELECT id, nome FROM produtos WHERE id = 100";
 
-//Preparar a QUERY
+// Preparar a QUERY
 $result_produtos = $conn->prepare($query_produtos);
 
-
-//Executar a QUERY com PDO
+// Executar a QUERY com PDO
 $result_produtos->execute();
 
-if(($result_produtos) and ($result_produtos->rowCount() != 0)){
-    //Ler os registros retornado do banco de dados
-    $produtos = $result_produtos->fetchAll();
+// Acessa o IF quando encontrou produto no banco de dados
+if (($result_produtos) and ($result_produtos->rowCount() != 0)) {
 
-    //retornar os dados em formato JSON
-    echo json_encode(['status' => 200, 'dados' => $produtos]);
-}else{
-    //retornar os dados em formato JSON
-    echo json_encode(['status' => 404, 'msg' => "Erro: nenhum produto encontrado!"]);
+    // Ler os registros retornado do banco de dados
+    while ($row_produto = $result_produtos->fetch(PDO::FETCH_ASSOC)) {
+        //var_dump($row_produto);
+        extract($row_produto);
+
+        // Acrescentar TAG filha <produto> no XML para colocar os dados do produto
+        $produtoXML = $produtosXML->addChild('produto');
+
+        // Acrescentar as tag no XML com os dados do produto
+        $produtoXML->addChild('id', $id);
+        $produtoXML->addChild('nome', $nome);
+    }
+
+    // Imprimir o XML
+    echo $produtosXML->asXML();
+
+    // Pausar o processamento
+    exit();
+} else {
+    // Criar a TAG filha <erro></erro> com a mensagem de erro
+    $produtosXML->addChild('erro', "Erro: Nenhum produto encontrado!");
+
+    // Imprimir o XML
+    echo $produtosXML->asXML();
+
+    // Pausar o processamento
+    exit();
 }
-
-
 
 
 
--- index.php
+++ index.php
@@ -1,14 +1,56 @@
 <?php 
 
 
+// URL da API
+$url = "http://localhost/curl_api-php/api/index.php";
+
+// A função curl_init() inicializa uma nova sessão 
+$ch = curl_init();
+
+// Utilizado CURLOPT_RETURNTRANSFER para esperar a resposta da URL
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, $url);
+
+// Enviar a requisição
+curl_setopt($ch, CURLOPT_URL, $url);
+
+// Executar solicitação de curl
+$dados = curl_exec($ch);
+
+// Fecha uma sessão cURL e libera todos os recursos
+curl_close($ch);
+
+// Imprimir o XML
+//echo $dados;
+
+// Transformar o conteúdo XML em Objeto
+$xml = simplexml_load_string($dados);
+
+// Acessa o IF quando encontrar produto
+if (isset($xml->produto)) {
+
+    // Percorre todos os registros de vendas
+    foreach ($xml->produto as $produto) :
+
+        // Imprimir as informações do XML
+        echo "Id do produto: " . $produto->id . "<br>";
+        echo "Nome do produto: " . $produto->nome . "<br>";
+
+        echo "<hr>";
+    endforeach;
+}
+
+// Acessa o IF quando encontrar erro
+if (isset($xml->erro)) {
+
+    // Imprimir a mensagem de erro
+    echo $xml->erro;
+}
 
 
 
 
-
-
-
-
+//http://localhost/curl_api-php/api/index.php
+//http://localhost/curl_api-php/index.php
 
 
 ?>
